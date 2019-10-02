e87c72352ce285c5d3d5859f10e1ea8ac6f06f9a
victor <victor@ieselcaminas.org>
2019-10-02 16:36:22 +0200

    xxx

--- app/src/main/java/org/ieselcaminas/victor/aboutme/MainActivity.kt
+++ app/src/main/java/org/ieselcaminas/victor/aboutme/MainActivity.kt
@@ -1,7 +1,14 @@
 package org.ieselcaminas.victor.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 data class Customer(val id: Long, val name: String)
 
@@ -11,8 +18,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var customer = Customer(1, "Schin")
-        var customer2 = customer.copy(name = "fffffff")
+        findViewById<Button>(R.id.done_button).setOnClickListener() {
+            addNickname(it)
+        }
+    }
 
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
